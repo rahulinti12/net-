1bfd60d0e3cea07bfd846c42d443898d75d66445
rahul12 <rahulopsn@gmail.com>
2019-04-04 20:33:47 +0530

    test

---
+++ src/Components/Server/src/Circuits/DefaultCircuitFactory.cs
@@ -0,0 +1,117 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Components.Browser;
+using Microsoft.AspNetCore.Components.Browser.Rendering;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.AspNetCore.Components.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.JSInterop;
+
+namespace Microsoft.AspNetCore.Components.Server.Circuits
+{
+    internal class DefaultCircuitFactory : CircuitFactory
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+        private readonly ILoggerFactory _loggerFactory;
+
+        public DefaultCircuitFactory(
+            IServiceScopeFactory scopeFactory,
+            ILoggerFactory loggerFactory)
+        {
+            _scopeFactory = scopeFactory ?? throw new ArgumentNullException(nameof(scopeFactory));
+            _loggerFactory = loggerFactory;
+        }
+
+        public override CircuitHost CreateCircuitHost(
+            HttpContext httpContext,
+            CircuitClientProxy client,
+            string uriAbsolute,
+            string baseUriAbsolute)
+        {
+            var components = ResolveComponentMetadata(httpContext, client);
+
+            var scope = _scopeFactory.CreateScope();
+            var encoder = scope.ServiceProvider.GetRequiredService<HtmlEncoder>();
+            var jsRuntime = (RemoteJSRuntime)scope.ServiceProvider.GetRequiredService<IJSRuntime>();
+            var componentContext = (RemoteComponentContext)scope.ServiceProvider.GetRequiredService<IComponentContext>();
+            jsRuntime.Initialize(client);
+            componentContext.Initialize(client);
+
+            var uriHelper = (RemoteUriHelper)scope.ServiceProvider.GetRequiredService<IUriHelper>();
+            if (client.Connected)
+            {
+                uriHelper.AttachJsRuntime(jsRuntime);
+                uriHelper.InitializeState(
+                    uriAbsolute,
+                    baseUriAbsolute);
+            }
+            else
+            {
+                uriHelper.InitializeState(uriAbsolute, baseUriAbsolute);
+            }
+
+            var rendererRegistry = new RendererRegistry();
+            var dispatcher = Renderer.CreateDefaultDispatcher();
+            var renderer = new RemoteRenderer(
+                scope.ServiceProvider,
+                rendererRegistry,
+                jsRuntime,
+                client,
+                dispatcher,
+                encoder,
+                _loggerFactory.CreateLogger<RemoteRenderer>());
+
+            var circuitHandlers = scope.ServiceProvider.GetServices<CircuitHandler>()
+                .OrderBy(h => h.Order)
+                .ToArray();
+
+            var circuitHost = new CircuitHost(
+                scope,
+                client,
+                rendererRegistry,
+                renderer,
+                components,
+                dispatcher,
+                jsRuntime,
+                circuitHandlers,
+                _loggerFactory.CreateLogger<CircuitHost>());
+
+            // Initialize per - circuit data that services need
+            (circuitHost.Services.GetRequiredService<ICircuitAccessor>() as DefaultCircuitAccessor).Circuit = circuitHost.Circuit;
+
+            return circuitHost;
+        }
+
+        internal static IList<ComponentDescriptor> ResolveComponentMetadata(HttpContext httpContext, CircuitClientProxy client)
+        {
+            if (!client.Connected)
+            {
+                // This is the prerendering case. Descriptors will be registered by the prerenderer.
+                return new List<ComponentDescriptor>();
+            }
+            else
+            {
+                var endpointFeature = httpContext.Features.Get<IEndpointFeature>();
+                var endpoint = endpointFeature?.Endpoint;
+                if (endpoint == null)
+                {
+                    throw new InvalidOperationException(
+                        $"{nameof(ComponentHub)} doesn't have an associated endpoint. " +
+                        "Use 'app.UseEndpoints(endpoints => endpoints.MapComponentHub<App>(\"app\"))' to register your hub.");
+                }
+
+                var componentsMetadata = endpoint.Metadata.OfType<ComponentDescriptor>().ToList();
+
+                return componentsMetadata;
+            }
+        }
+    }
+}
---
+++ src/Components/Server/src/Circuits/RemoteUriHelper.cs
@@ -0,0 +1,96 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.AspNetCore.Components.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.JSInterop;
+using Interop = Microsoft.AspNetCore.Components.Browser.BrowserUriHelperInterop;
+
+namespace Microsoft.AspNetCore.Components.Server.Circuits
+{
+    /// <summary>
+    /// A Server-Side Components implementation of <see cref="IUriHelper"/>.
+    /// </summary>
+    public class RemoteUriHelper : UriHelperBase
+    {
+        private IJSRuntime _jsRuntime;
+        private readonly ILogger<RemoteUriHelper> _logger;
+
+        public RemoteUriHelper(ILogger<RemoteUriHelper> logger)
+        {
+            _logger = logger;
+        }
+
+        public bool HasAttachedJSRuntime => _jsRuntime != null;
+
+        /// <summary>
+        /// Initializes the <see cref="RemoteUriHelper"/>.
+        /// </summary>
+        /// <param name="uriAbsolute">The absolute URI of the current page.</param>
+        /// <param name="baseUriAbsolute">The absolute base URI of the current page.</param>
+        /// <param name="jsRuntime">The <see cref="IJSRuntime"/> to use for interoperability.</param>
+        public override void InitializeState(string uriAbsolute, string baseUriAbsolute)
+        {
+            base.InitializeState(uriAbsolute, baseUriAbsolute);
+            TriggerOnLocationChanged();
+        }
+
+        /// <summary>
+        /// Initializes the <see cref="RemoteUriHelper"/>.
+        /// </summary>
+        /// <param name="uriAbsolute">The absolute URI of the current page.</param>
+        /// <param name="baseUriAbsolute">The absolute base URI of the current page.</param>
+        /// <param name="jsRuntime">The <see cref="IJSRuntime"/> to use for interoperability.</param>
+        internal void AttachJsRuntime(IJSRuntime jsRuntime)
+        {
+            if (_jsRuntime != null)
+            {
+                throw new InvalidOperationException("JavaScript runtime already initialized.");
+            }
+            _jsRuntime = jsRuntime;
+            _jsRuntime.InvokeAsync<object>(
+                    Interop.EnableNavigationInterception,
+                    typeof(RemoteUriHelper).Assembly.GetName().Name,
+                    nameof(NotifyLocationChanged));
+
+            _logger.LogInformation($"{nameof(RemoteUriHelper)} initialized.");
+        }
+
+        /// <summary>
+        /// For framework use only.
+        /// </summary>
+        [JSInvokable(nameof(NotifyLocationChanged))]
+        public static void NotifyLocationChanged(string uriAbsolute)
+        {
+            var circuit = CircuitHost.Current;
+            if (circuit == null)
+            {
+                var message = $"{nameof(NotifyLocationChanged)} called without a circuit.";
+                throw new InvalidOperationException(message);
+            }
+
+            var uriHelper = (RemoteUriHelper)circuit.Services.GetRequiredService<IUriHelper>();
+
+            uriHelper.SetAbsoluteUri(uriAbsolute);
+
+            uriHelper._logger.LogDebug($"Location changed to '{uriAbsolute}'.");
+            uriHelper.TriggerOnLocationChanged();
+        }
+
+        protected override void NavigateToCore(string uri, bool forceLoad)
+        {
+            _logger.LogDebug($"Log debug {uri} force load {forceLoad}.");
+
+            if (_jsRuntime == null)
+            {
+                throw new InvalidOperationException("Navigation commands can not be issued at this time. This is because the component is being " +
+                    "prerendered and the page has not yet loaded in the browser or because the circuit is currently disconnected. " +
+                    "Components must wrap any navigation calls in conditional logic to ensure those navigation calls are not " +
+                    "attempted during prerendering or while the client is disconnected.");
+            }
+            _jsRuntime.InvokeAsync<object>(Interop.NavigateTo, uri, forceLoad);
+        }
+    }
+}
